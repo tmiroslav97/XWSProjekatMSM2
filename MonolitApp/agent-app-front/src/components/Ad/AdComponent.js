276b5749b918092d4544613df3e5ac7dedb9a1be
Miroslav Tomic <56967120+tmiroslav97@users.noreply.github.com>
2020-06-21 23:57:31 +0200

    Fixing bugs

--- MonolitApp/agent-app-front/src/components/Ad/AdComponent.js
+++ MonolitApp/agent-app-front/src/components/Ad/AdComponent.js
@@ -6,87 +6,87 @@ import { Row, Col, Card, Button } from 'react-bootstrap';
 const AdComponent = (props) => {
 
     var disable = false;
-    if(props.token === null){
+    if (props.token === null) {
         disable = true;
     }
 
     return (
 
         props.ads.map((variant, idx) => (
-           
-              
-                <Card
-                    key={idx}
-                    border="secondary"
-                    className="mt-5"
-                >
-                    <Card.Body>
-                        <Card.Title>{variant.name}</Card.Title>
-                        <Card.Subtitle className="mb-2 text-muted">{variant.carManufacturer} {variant.carModel}  </Card.Subtitle>
-                        <Row>
-                            <Col md={4}>
-                                <Card.Img src="/img-ad/fiat.jpg" />
-                            </Col>
-                            <Col >
-                                <br />
-                                <Row>
-                                    <Col sm={2}>
-                                        <Card.Img src="/img-icon/seat-num.png" />
-                                    </Col>
-                                    <Card.Text>
-                                        {variant.childrenSeatNum}
-                                    </Card.Text>
-                                </Row>
-                                <Row>
-                                    <Col sm={2}>
-                                        <Card.Img src="/img-icon/location.png" />
-                                    </Col>
-                                    <Card.Text>
-                                        {variant.location}
-                                    </Card.Text>
-                                </Row>
-                                <Row>
-                                    <Col sm={2}>
-                                        <Card.Img src="/img-icon/price-tag.png" />
-                                    </Col>
-                                    <Card.Text>
-                                        {variant.price} $
+
+
+            <Card
+                key={idx}
+                border="secondary"
+                className="mt-5"
+            >
+                <Card.Body>
+                    <Card.Title>{variant.name}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">{variant.carManufacturer} {variant.carModel}  </Card.Subtitle>
+                    <Row>
+                        <Col md={4}>
+                            <Card.Img src={"/img-ad/" + variant.coverPhoto} />
+                        </Col>
+                        <Col >
+                            <br />
+                            <Row>
+                                <Col sm={2}>
+                                    <Card.Img src="/img-icon/seat-num.png" />
+                                </Col>
+                                <Card.Text>
+                                    {variant.childrenSeatNum}
+                                </Card.Text>
+                            </Row>
+                            <Row>
+                                <Col sm={2}>
+                                    <Card.Img src="/img-icon/location.png" />
+                                </Col>
+                                <Card.Text>
+                                    {variant.location}
+                                </Card.Text>
+                            </Row>
+                            <Row>
+                                <Col sm={2}>
+                                    <Card.Img src="/img-icon/price-tag.png" />
+                                </Col>
+                                <Card.Text>
+                                    {variant.price} $
                                         </Card.Text>
-                                </Row>
+                            </Row>
 
-                            </Col>
-                            <Col>
-                                <br />
-                                <Row>
-                                    <Col sm={2}>
-                                        <Card.Img src="/img-icon/fuel24.png" />
-                                    </Col>
-                                    <Card.Text>
-                                        {variant.fuelType}
-                                    </Card.Text>
-                                </Row>
-                                <Row>
-                                    <Col sm={2}>
-                                        <Card.Img src="/img-icon/km.png" />
-                                    </Col>
-                                    <Card.Text>
-                                        {variant.mileage}
-                                    </Card.Text>
-                                </Row>
-                                <Card.Link href={'/agent-firm/ad-detail-view/' + variant.id} >Vise detalja </Card.Link>
+                        </Col>
+                        <Col>
+                            <br />
+                            <Row>
+                                <Col sm={2}>
+                                    <Card.Img src="/img-icon/fuel24.png" />
+                                </Col>
+                                <Card.Text>
+                                    {variant.fuelType}
+                                </Card.Text>
+                            </Row>
+                            <Row>
+                                <Col sm={2}>
+                                    <Card.Img src="/img-icon/km.png" />
+                                </Col>
+                                <Card.Text>
+                                    {variant.mileage}
+                                </Card.Text>
+                            </Row>
+                            <Card.Link href={'/agent-firm/ad-detail-view/' + variant.id} >Vise detalja </Card.Link>
 
-                            </Col>
-                              
-                            <Col>
-                                {
-                                    disable ?  null :  <Button variant="outline-success" >Dodaj u korpu</Button>
-                                }
-                            </Col>
-                        
-                        </Row>
-                    </Card.Body>
-                </Card>
-        
+                        </Col>
+
+                        <Col>
+                            {
+                                disable ? null : <Button variant="outline-success" >Dodaj u korpu</Button>
+                            }
+                        </Col>
+
+                    </Row>
+                </Card.Body>
+            </Card>
+
         ))
 
     );
