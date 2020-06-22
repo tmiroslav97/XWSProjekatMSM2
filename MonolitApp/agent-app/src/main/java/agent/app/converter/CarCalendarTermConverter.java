7a486e4bc407352c351e01dd62d26f74ca3bed1d
Svetlana <antesevicceca@gmail.com>
2020-06-23 00:57:58 +0200

    Implemented controller and service methods for car calendar in monolit app.

--- MonolitApp/agent-app/src/main/java/agent/app/converter/CarCalendarTermConverter.java
+++ MonolitApp/agent-app/src/main/java/agent/app/converter/CarCalendarTermConverter.java
@@ -1,7 +1,12 @@
 package agent.app.converter;
 
 import agent.app.dto.car.CarCalendarTermCreateDTO;
+import agent.app.dto.car.CarCalendarTermDTO;
 import agent.app.model.CarCalendarTerm;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class CarCalendarTermConverter {
 
@@ -12,4 +17,14 @@ public class CarCalendarTermConverter {
                 .endDate(DateAPI.dateStringToDateTime(carCalendarTermCreateDTO.getEndDate()))
                 .build();
     }
+
+    public static CarCalendarTerm toCarCalendarTermFromRequest(CarCalendarTermDTO carCalendarTermDTO){
+
+        return CarCalendarTerm.builder()
+                .startDate(DateAPI.dateStringToDateTime(carCalendarTermDTO.getStartDate()))
+                .endDate(DateAPI.dateStringToDateTime(carCalendarTermDTO.getEndDate()))
+                .build();
+    }
+
+
 }
