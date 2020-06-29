e24fdac4da38cf340375a22070458772d79aa5ea
Miroslav Tomic <56967120+tmiroslav97@users.noreply.github.com>
2020-06-29 18:02:12 +0200

    Adding spinner container to sync container

--- MonolitApp/agent-app-front/src/containers/AgentFirm/SynchronizeContainer.js
+++ MonolitApp/agent-app-front/src/containers/AgentFirm/SynchronizeContainer.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
-import SynchronizeComponent from '../../components/AgentFirm/SynchronizeComponent';
 import { useDispatch } from 'react-redux';
-import AdServices from '../../services/AdServices';
+import { Container } from 'react-bootstrap';
 import { putSuccessMsg, putErrorMsg } from '../../store/common/actions';
+import AdServices from '../../services/AdServices';
+import SynchronizeComponent from '../../components/AgentFirm/SynchronizeComponent';
+import SpinnerContainer from '../Common/SpinnerContainer';
 
 const SynchronizeContainer = () => {
     const dispatch = useDispatch();
     const [validated, setValidated] = useState(false);
+    const [isSync, setIsSync] = useState(false);
 
     const handleSync = (event) => {
         event.preventDefault();
@@ -23,16 +26,23 @@ const SynchronizeContainer = () => {
     };
 
     const syncData = async (payload) => {
+        setIsSync(true);
         const result = await AdServices.syncData(payload);
         if (result === 'Sinhronizacija uspjesno obavljena.') {
             dispatch(putSuccessMsg(result));
         } else {
             dispatch(putErrorMsg(result));
         }
+        setIsSync(false);
     }
 
     return (
-        <SynchronizeComponent onSubmit={handleSync} validated={validated} />
+        <Container>
+            { !isSync ?
+                <SynchronizeComponent onSubmit={handleSync} validated={validated} />
+                : <SpinnerContainer />
+            }
+        </Container>
     )
 
 }
