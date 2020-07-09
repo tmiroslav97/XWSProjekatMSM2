dfdf765aa955fea21a9563543c73df583880e640
Miroslav Tomic <56967120+tmiroslav97@users.noreply.github.com>
2020-07-09 15:46:08 +0200

    Adding message model

--- Services/message-service/src/main/java/services/app/messageservice/common/db/DbTableConstants.java
+++ Services/message-service/src/main/java/services/app/messageservice/common/db/DbTableConstants.java
@@ -2,4 +2,6 @@ package services.app.messageservice.common.db;
 
 public class DbTableConstants {
     public static final String MESSAGE = "message";
+    public static final String CONVERSATION = "conversation";
+    public static final String CONVMSG = "convMsg";
 }
---
+++ Services/message-service/src/main/java/services/app/messageservice/converter/DateAPI.java
@@ -0,0 +1,68 @@
+package services.app.messageservice.converter;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+
+public class DateAPI {
+    public static DateTime DateTimeNow() {
+        String dateStr = DateTimeToStringDateTime(new DateTime());
+        DateTimeFormatter dtf = DateTimeFormat.forPattern("HH:mm dd-MM-yyyy");
+        return dtf.withZone(DateTimeZone.UTC).parseDateTime(dateStr);
+    }
+
+    public static String DateTimeToStringDateTime(DateTime dateTime) {
+        DateTimeFormatter dtf = DateTimeFormat.forPattern("HH:mm dd-MM-yyyy");
+        return dtf.print(dateTime);
+    }
+
+    public static DateTime DateTimeStringToDateTime(String dateStr) {
+        DateTimeFormatter dtf = DateTimeFormat.forPattern("HH:mm dd-MM-yyyy");
+        return dtf.withZone(DateTimeZone.UTC).parseDateTime(dateStr);
+    }
+
+    //za datume sa fronta koji stizu u formatu: 2020-06-03T03:03
+    public static DateTime DateStringToDateTimeFromFronted(String dateStr) {
+        DateTimeFormatter dtf = DateTimeFormat.forPattern("HH:mm dd-MM-yyyy");
+
+        String[] datumSplitted = dateStr.split("-");
+        String yyyy = datumSplitted[0];
+        String MM = datumSplitted[1];
+        String dd = datumSplitted[2];
+
+        String hh = "00";
+        String mm = "00";
+
+        String newDateStr = hh + ":" + mm + " " + dd + "-" + MM + "-" + yyyy;
+
+        DateTime dateTime = dtf.withZone(DateTimeZone.UTC).parseDateTime(newDateStr);
+
+        return dateTime;
+    }
+
+    //za datume i vrijeme sa fronta koji stizu u formatu: 2020-06-03T03:03
+    public static DateTime DateTimeStringToDateTimeFromFronted(String dateTimeStr) {
+        DateTimeFormatter dtf = DateTimeFormat.forPattern("HH:mm dd-MM-yyyy");
+
+        String[] res = dateTimeStr.split("T");
+
+        String datum = res[0];
+        String[] datumSplitted = datum.split("-");
+        String yyyy = datumSplitted[0];
+        String MM = datumSplitted[1];
+        String dd = datumSplitted[2];
+
+        String vrijeme = res[1];
+        String[] vrijemeSpplited = vrijeme.split(":");
+        String hh = vrijemeSpplited[0];
+        String mm = vrijemeSpplited[1];
+
+        String newDateStr = hh + ":" + mm + " " + dd + "-" + MM + "-" + yyyy;
+
+        DateTime dateTime = dtf.withZone(DateTimeZone.UTC).parseDateTime(newDateStr);
+
+        return dateTime;
+    }
+}
