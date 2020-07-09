3ca7a108f79e88eedb3a49825305432076baf319
Magdalena <57190427+magdalenalakic@users.noreply.github.com>
2020-07-10 00:55:45 +0200

    Added method in controller

--- Services/ad-service/src/main/java/services/app/adservice/controller/AdController.java
+++ Services/ad-service/src/main/java/services/app/adservice/controller/AdController.java
@@ -14,6 +14,8 @@ import services.app.adservice.dto.ad.ReversePricelistDTO;
 import services.app.adservice.model.CustomPrincipal;
 import services.app.adservice.service.intf.AdService;
 
+import java.security.Principal;
+
 
 @RestController
 @RequestMapping(value = "/ad", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -120,4 +122,44 @@ public class AdController {
     public ResponseEntity<?> removeDiscountToAd(@RequestParam("discountId") Long discountId, @RequestParam("adId") Long adId) {
         return new ResponseEntity<>(adService.removeDiscountToAd(discountId, adId), HttpStatus.OK);
     }
+
+    @PreAuthorize("hasAuthority('ROLE_AGENT')")
+    @RequestMapping(value = "/best-average-grade", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> findBestAverageGradeAd() {
+        System.out.println("Best average grade");
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        CustomPrincipal principal = (CustomPrincipal) auth.getPrincipal();
+        System.out.println(principal.getUserId());
+        Long publisher_id = Long.parseLong(principal.getUserId());
+        System.out.println(publisher_id);
+        return new ResponseEntity<>(adService.findBestAverageGrade(publisher_id), HttpStatus.OK);
+
+    }
+
+    @PreAuthorize("hasAuthority('ROLE_AGENT')")
+    @RequestMapping(value = "/max-mileage", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> findMaxMileageAd() {
+        System.out.println("Max mileage contoller");
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        CustomPrincipal principal = (CustomPrincipal) auth.getPrincipal();
+        System.out.println(principal.getUserId());
+        Long publisher_id = Long.parseLong(principal.getUserId());
+        System.out.println(publisher_id);
+        return new ResponseEntity<>(adService.findMaxMileage(publisher_id), HttpStatus.OK);
+
+    }
+
+    @PreAuthorize("hasAuthority('ROLE_AGENT')")
+    @RequestMapping(value = "/max-comments", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> findMaxCommentsAd() {
+        System.out.println("Max comments contoller");
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        CustomPrincipal principal = (CustomPrincipal) auth.getPrincipal();
+        System.out.println(principal.getUserId());
+        Long publisher_id = Long.parseLong(principal.getUserId());
+        System.out.println(publisher_id);
+        return new ResponseEntity<>(adService.findMaxComment(publisher_id), HttpStatus.OK);
+
+    }
+
 }
