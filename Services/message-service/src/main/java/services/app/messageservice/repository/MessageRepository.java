eecb626763e2a0c14a50940f6c2302dadca3fa61
Miroslav Tomic <56967120+tmiroslav97@users.noreply.github.com>
2020-07-09 16:30:40 +0200

    Implemented initial message servie methods

--- Services/message-service/src/main/java/services/app/messageservice/repository/MessageRepository.java
+++ Services/message-service/src/main/java/services/app/messageservice/repository/MessageRepository.java
@@ -3,5 +3,9 @@ package services.app.messageservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import services.app.messageservice.model.Message;
 
+import java.util.List;
+
 public interface MessageRepository extends JpaRepository<Message, Long> {
+
+    List<Message> findByConversationId(Long id);
 }
